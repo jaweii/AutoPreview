8cf59b6e3fb9faa3da068d061885009e5f68b71d
jawei <1005945485@qq.com>
2022-05-10 17:23:09 +0800

    feat: mixpanel

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./src/App";
+import mixpanel from "mixpanel-browser";
+
+mixpanel.init("4fceeaac8b239745acb694f47076cc8a");
+mixpanel.track("active");
 
 ReactDOM.render(<App />, document.querySelector("#app"));
